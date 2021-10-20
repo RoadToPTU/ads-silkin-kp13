bfbc9c7babb8deaf8e683ad96a957342dced713c
unknown <comradesergeant@gmail.com>
2021-10-20 23:34:34 +0300

    end task

---
+++ lab1/prj1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+            double a;
+            double b;
+            if (z == 0)
+            {
+                Console.WriteLine("error");
+            }
+            else
+            {
+                a = Math.Cos(x + x * y / z);
+                if (Math.Cos(a) == 0)
+                {
+                    Console.WriteLine("error");
+                }
+                else
+                {
+                    b = Math.Pow(x, 3)/Math.Cos(a);
+                    Console.WriteLine("a = " + a);
+                    Console.WriteLine("b = " + b);
+                }
+                
+            }
+           
+            
+        }
+    }
+}
